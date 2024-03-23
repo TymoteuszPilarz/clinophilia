401ceca7829bb6c2dc375cd0ecffab8943990c61
Sebastian Pilarz <pilarz.sebastian@gmail.com>
2024-03-23 06:50:16 +0100

    Added teams controller

---
+++ constants.py
@@ -0,0 +1,10 @@
+from os import path as p
+
+PROJECT_DIR_ABS_PATH = p.abspath(p.realpath(p.dirname(__file__)))
+RESOURCES_DIR_ABS_PATH = p.join(PROJECT_DIR_ABS_PATH, 'resources')
+TEAMS_LEAVE_BUTTON_ABS_PATH = p.join(RESOURCES_DIR_ABS_PATH,
+                                     'teams_leave_button.png')
+TEAMS_PEOPLE_ICON_ABS_PATH = p.join(RESOURCES_DIR_ABS_PATH,
+                                    'teams_people_icon.png')
+TEAMS_SHIELD_ICON_ABS_PATH = p.join(RESOURCES_DIR_ABS_PATH,
+                                    'teams_shield_icon.png')
